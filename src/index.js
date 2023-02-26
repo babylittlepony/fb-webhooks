050b4c4d723ec5a52740884eed9217df2aa5063e
Egar <pramana.egar1540@gmail.com>
2023-02-26 21:03:07 +0700

    delete comment

--- src/index.js
+++ src/index.js
@@ -19,5 +19,3 @@ app.use(router)
 app.listen(3000, () => {
   console.log("Server on Port 3000")
 })
-
-// curl -X GET "localhost:3000/webhook?hub.verify_token=EAAHZAixIRsU4BADEgiqFCCr9trfUin576pa2tqbRe2LvcZAtVyteX6ci6LlZBx3rh16JV1quBJ1TysKXZBwK4fcYfGhnS6EwBvua4ZBBdFPe0NnZAhrEL2cM4LMxgZC1a8h5Gm47PuXT8YJnyvLo8umhw37ZARG9Gpn5CMVq98mgIzolpTgZAoSeD&hub.challenge=CHALLENGE_ACCEPTED&hub.mode=subscribe"
